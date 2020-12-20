296658f92a23e8c3df5132792f88980bf7f359a3
Sultan <sultanp@gmail.com>
2020-12-20 20:13:24 +0000

    initial game logic created

--- app.js
+++ app.js
@@ -3,3 +3,57 @@ let gameOptions = ["rock", "paper", "scissors"];
 let computerPlay = function (){
 return gameOptions[Math.floor(Math.random()*3)]
 }
+
+let playerSelection = function () {
+    prompt("Rock? Paper? Scissors? Which will it be?");
+}
+
+let playRound = function () {
+    let playerSelection = prompt("rock, paper, scissors?");
+
+    if(playerSelection === "rock"){
+        switch(computerPlay()){
+            case "rock": 
+                console.log("draw");
+                break;
+            case "paper":
+                console.log("paper beats rock");
+                break;
+            case "scissors":
+                console.log("rock beats paper");
+        }
+    } else if(playerSelection === "paper"){
+        switch(computerPlay()){
+            case "rock": 
+                console.log("paper beats rock");
+                break;
+            case "paper":
+                console.log("draw");
+                break;
+            case "scissors":
+                console.log("scissors beats paper");
+        } 
+    }
+        
+        else if(playerSelection === "scissors"){
+            switch(computerPlay()){
+                case "rock": 
+                    console.log("rock beats scissors");
+                    break;
+                case "paper":
+                    console.log("drawscissors beats paper");
+                    break;
+                case "scissors":
+                    console.log("draw");
+            }
+    
+        } else {
+            console.log("invalid option");
+        }
+    
+
+    
+
+   
+
+}
