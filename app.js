c54dcca8d7a08ab2e7bf3a6a2c4bd6ab44880777
Sultan <sultanp@gmail.com>
2020-12-26 02:32:53 +0000

    finalised styling - JS needs refactoring

--- app.js
+++ app.js
@@ -7,17 +7,6 @@ return gameOptions[Math.floor(Math.random()*3)]
 let playerScore = 0;
 let computerScore = 0;
 
-let game = function(e){
-    while(playerScore < 3 && computerScore < 3){     
-       return playRound(e.target.id)
-    } if(playerScore === 3){
-        return "Congrats you win!!!"
-    } else if (computerScore === 3
-        ){
-        return "YOU LOSE"
-    }
-}
-
 let gameOver = false;
 
 const rock = document.querySelector("#rock");
@@ -27,6 +16,7 @@ const resetButton = document.querySelector("#reset");
 
 let playerDisplay = document.querySelector("#playerdisplay");
 let computerDisplay = document.querySelector("#computerdisplay");
+let gameDisplay = document.querySelector("#gamestatus");
 
 let displayScore = () => {
     playerDisplay.textContent = playerScore;
@@ -38,43 +28,71 @@ resetButton.addEventListener("click", () => {
     computerScore = 0;
     gameOver = false;
     displayScore();
+    gameDisplay.textContent = "Rock, Paper, Scissors!";
 })
 
 rock.addEventListener("click", (e) => {
-        
         if(!gameOver){
-            console.log(playRound(`${e.target.id}`));
-            displayScore();
+            playRound(`${e.target.id}`);
             if(playerScore > 4){
                 gameOver = true;
+                displayScore();
+               return gameDisplay.textContent = "YOU WIN";
             } else if(computerScore > 4){
                 gameOver = true;
+                displayScore();
+                return gameDisplay.textContent = "YOU LOSE";
             }
+            return displayScore();
         }   
 });
 
+let gamePlay = function (e) {
+    if(!gameOver){
+        console.log(playRound(`${e.target.id}`));
+        displayScore();
+        if(playerScore > 4){
+            gameOver = true;
+            gameDisplay.textContent = "YOU WIN";
+        } else if(computerScore > 4){
+            gameOver = true;
+            gameDisplay.textContent = "YOU LOSE";
+        }
+    } 
+
+}
+
 paper.addEventListener("click", (e) => {
     if(!gameOver){
-        console.log(playRound(`${e.target.id}`));
-        displayScore();
+        playRound(`${e.target.id}`);
         if(playerScore > 4){
             gameOver = true;
+            displayScore();
+           return gameDisplay.textContent = "YOU WIN";
         } else if(computerScore > 4){
             gameOver = true;
+            displayScore();
+            return gameDisplay.textContent = "YOU LOSE";
         }
-    }  
+        return displayScore();
+    }   
 });
 
-scissors.addEventListener("click", (e) =>{
+
+scissors.addEventListener("click", (e) => {
     if(!gameOver){
-        console.log(playRound(`${e.target.id}`));
-        displayScore();
+        playRound(`${e.target.id}`);
         if(playerScore > 4){
             gameOver = true;
+            displayScore();
+           return gameDisplay.textContent = "YOU WIN";
         } else if(computerScore > 4){
             gameOver = true;
+            displayScore();
+            return gameDisplay.textContent = "YOU LOSE";
         }
-    }  
+        return displayScore();
+    }   
 });
 
 
@@ -83,36 +101,36 @@ let playRound = function (playerChoice) {
     const computerSelection = computerChoices();
 
     if(playerChoice === computerSelection){
-        return "Draw"
+        gameDisplay.textContent = "Draw"
     }   else if(playerChoice === "rock"){
         switch(computerSelection){
             case "paper":
                 computerScore++;
-                return "YOU LOSE! Paper beats rock";
+                gameDisplay.textContent = "YOU LOSE! Paper beats rock";
                 break;
             case "scissors":
                 playerScore++;
-                return "YOU WIN! Rock beats scissors";
+                gameDisplay.textContent = "YOU WIN! Rock beats scissors";
         }
     }   else if(playerChoice === "paper"){
         switch(computerSelection){
             case "rock": 
                 playerScore++;           
-                return "YOU WIN. Paper beats rock";
+                gameDisplay.textContent = "YOU WIN. Paper beats rock";
                 break;
             case "scissors":
                 computerScore++;
-                return "YOU LOSE! Scissors beats paper";
+                gameDisplay.textContent = "YOU LOSE! Scissors beats paper";
         } 
     }   else if(playerChoice === "scissors"){
             switch(computerSelection){
                 case "rock": 
                     computerScore++;
-                    return "YOU LOSE! Rock beats scissors";
+                    gameDisplay.textContent = "YOU LOSE! Rock beats scissors";
                     break;
                 case "paper":
                     playerScore++;
-                    return "YOU WIN! Scissors beats paper";
+                    gameDisplay.textContent = "YOU WIN! Scissors beats paper";
             }
     } 
 }
